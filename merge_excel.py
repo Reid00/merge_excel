adbe11a5e42393b58ecfd831c19edc2731998be9
Baoli Zhang (Beyondsoft Corporation) <v-baoz@microsoft.com>
2020-01-01 22:59:35 +0800

    add support many extension in the same time

--- merge_excel.py
+++ merge_excel.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from pathlib import Path
 import logging
+from itertools import chain
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 class MergeExcel:
@@ -11,17 +12,28 @@ class MergeExcel:
         self.path = Path(path)
         self.sum=sum
 
-    def get_files(self, ext='xlsx'):
-        self.ext = ext
-        # 获取当前目录下的所有xslx 文件,返回是个generate
-        files = self.path.glob(f'*.{self.ext}')
+    def files_path(self,exts=['xlsx','xls']):
+        files=list()
+        self.exts=exts
+        for ext in exts:
+                    # 获取当前目录下的所有xslx 文件,返回是个generate
+            files.append(self.path.glob(f'*.{ext}'))
+        files=list(chain(*files))
+        return files
+
+    def get_content(self):
+        files=self.files_path()
         all_cont = pd.DataFrame()
         usecols= int(input('\033[1;36m please input the Column Number you want to use for this action:\033[0m').strip())
         usecols=range(usecols)
         for file in files:
             print('======='*10)
             logging.info(f'file name: {file}')
-            # 读取每个excel 的各个sheet
+            # 读取每个excel 的各个sheetDefaults ： 第一页作为数据文件
+            # 1 ：第二页作为数据文件
+            # “Sheet1” ：第一页作为数据文件
+            # [0,1，“SEET5”] ：第一、第二和第五作为作为数据文件
+            # None ：所有表为作为数据文件
             content = pd.read_excel(file, sheet_name=None, encoding='utf8')
             # 获取所有的sheet name
             sheet_names = content.keys()
@@ -35,18 +47,18 @@ class MergeExcel:
                 sheet_name_inputs = input('\033[1;36m please input the sheetnames you want to merge,split with ",":\033[0m').split(',')
                 for sheet_name in sheet_name_inputs:
                     content = pd.read_excel(file, sheet_name=sheet_name.strip(), encoding='utf-8-sig',usecols=usecols)
-                    logging.info(f'file line number before: {content.shape[0]}')
+                    logging.info(f'Sheet {sheet_name} own line number before: {content.shape[0]}')
                     content=content.dropna(how='all')
-                    logging.info(f'the columns of this sheet: {content.columns}')
-                    logging.info(f'file line number after dropna: {content.shape[0]}')
+                    logging.info(f'Sheet {sheet_name} own columns: {content.columns}')
+                    logging.info(f'Sheet {sheet_name} own line number after dropna: {content.shape[0]}')
                     self.sum += content.shape[0]
                     all_cont = pd.concat([all_cont, content], axis=0, sort=False)
             else:
                 content = pd.read_excel(file, encoding='utf-8-sig',usecols=usecols)
-                logging.info(f'file line number before: {(content.shape[0])}')
+                logging.info(f'this file own line number before: {content.shape[0]}')
                 content=content.dropna(how='all')
-                logging.info(f'the columns of this sheet: {content.columns}')
-                logging.info(f'file line number after dropna: {(content.shape[0])}')
+                logging.info(f'this file own columns: {content.columns}')
+                logging.info(f'this file own line number after dropna: {content.shape[0]}')
                 self.sum += content.shape[0]
                 all_cont = pd.concat([all_cont, content], axis=0, sort=False)
         logging.info(f'\033[1;36m All the sum of content is: {self.sum} \033[0m')
@@ -54,10 +66,12 @@ class MergeExcel:
 
 
 if __name__ == '__main__':
-    me = MergeExcel(r'D:\download_D\1230_小万歌曲标注')
-    all_content = me.get_files()
+    me = MergeExcel(r'D:\download_D\1230_小万歌曲标注\test')
+    all_content = me.get_content()
     logging.info(f'\033[1;36m All content of merged shape is {all_content.shape}\033[0m')
-    output_name = Path(r'D:\download_D\1230_小万歌曲标注\res.csv')
+    # output_name = Path(r'D:\download_D\1230_小万歌曲标注\test\res.csv')
+    output_name = Path(r'D:\download_D\1230_小万歌曲标注\test\res.xlsx')
     if output_name.exists():
         output_name.unlink()
-    all_content.to_csv(output_name, index=None, header=True, mode='w', encoding='utf-8-sig')
+    # all_content.to_csv(output_name, index=None, header=True, mode='w', encoding='utf-8')
+    all_content.to_excel(output_name, index=None, header=True, encoding='utf-8')
